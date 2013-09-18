eb678c0af3202c842740e4ff568b83cfbed69b1f
NJJacob <njacob2@emich.edu>
2013-09-18 15:23:31 -0400

    Update Get-SecWAPS.ps1

--- Log-Management/Get-SecWAPS.ps1
+++ Log-Management/Get-SecWAPS.ps1
@@ -7,33 +7,30 @@ function Get-SecWAPs
     This works with CSIS Control 7 Wireless Device Control
     #>
 
+    [CmdletBinding()]
+    param(
+        [Switch]$CreateBaseline
+    )
+
     $computer = Get-Content Env:\COMPUTERNAME
-    $filename = Get-DateISO8601 -Prefix ".\$computer-WAP" -Suffix ".xml"
+    $filename = Get-DateISO8601 -Prefix ".\$computer-WAPS" -Suffix ".xml"
 
 
     netsh wlan show network | Export-Clixml $filename
 
-    if(-NOT(Test-Path ".\Baselines\$computer-WAP-Baseline.xml"))
+    if($CreateBaseline)
     {
         Rename-Item $filename "$computer-WAP-Baseline.xml"
         Move-Item ".\$computer-WAP-Baseline.xml" .\Baselines
-         Write-Warning "The baseline file for this computer has been created, running this script again."
-        Invoke-Expression $MyInvocation.MyCommand
+        if(Test-Path ".\Baselines\$computer-WAP-Baseline.xml"){
+   		Write-Warning "The baseline file for this computer has been created, running this script again."
+        	Invoke-Expression $MyInvocation.MyCommand
+        }
 	    
     }
-
-	else
+    else
     {
-        [System.Array]$approved = Import-Clixml -Path ".\Baselines\$computer-WAP-Baseline.xml"
-        [System.Array]$installed = Import-Clixml -Path $filename
-
-        Move-Item $filename .\Reports
-
-        [string]$filename = Get-DateISO8601 -Prefix ".\$computer-WAP-Exception-Report" -Suffix ".xml"
-        Compare-Object $approved $installed -Property | Export-Clixml  ".\Exception-Reports\$filename"
-
-        # The script can be emailed for review or processing in the ticketing system:
-        # Send-MailMessage -To -Subject "Wireless access point exception for $computer" -Body "The report is attached." -Attachments $filename
+        Compare-SecWAPs
     }
 
     
